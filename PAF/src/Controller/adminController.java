15b01a53f1541cbfd1f385ef09d848510ca6e610
Lihini <lihinipriyasarani123@gmail.com>
2021-04-20 00:41:14 +0530

    Admin crud operation added

---
+++ PAF/src/Controller/adminController.java
@@ -0,0 +1,351 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import DBUtil.DBConnection;
+import Model.Admin;
+import Model.Researcher;
+
+
+
+
+
+public class adminController  {
+	
+	DBConnection dbObj = new DBConnection();
+
+	//===================== View Orders ==========================
+	
+	
+//	public String viewAdmins() {
+//
+//		String output = "";
+//		
+//		
+//		Admin admin = new Admin();
+//		
+//		try {
+//			Connection con = dbObj.connect();
+//			if (con == null) {
+//				return "Error while connecting to the database for reading.";
+//			}
+//			// Prepare the html table to be displayed
+//			output = "<table border=\"1\"><tr><th>AdminID</th>" + "<th>Name</th> "
+//					+ "<th>Company</th> " + "<th>Address</th>" + "<th>Email</th>" + "<th>Phone</th>"  + "<th>Password</th>"  + "<th>Amount</th>"  +  "<th>Lisencenum</th></tr>";
+//
+//			String query = "select * from admin";
+//			Statement stmt = con.createStatement();
+//			ResultSet rs = stmt.executeQuery(query);
+//
+//			// iterate through the rows in the result set
+//			while (rs.next()) {
+//
+//				admin.setAid(rs.getInt("adminId"));
+//				admin.setName(rs.getString("name"));
+//				admin.setCompany(rs.getString("company"));
+//				admin.setAddress(rs.getString("address"));
+//				admin.setEmail(rs.getString("email"));
+//				admin.setPhone(rs.getString("phone"));
+//				admin.setPassword(rs.getString("password"));
+//				admin.setAmount(rs.getString("amount"));
+//				admin.setlisencenum(rs.getString("lisencenum"));
+//			
+//
+//				// Add into the html table
+//				output += "<tr><td>" + admin.getAid() + "</td>";
+//				output += "<td>" + admin.getName() + "</td>";
+//				output += "<td>" + admin.getCompany() + "</td>";
+//				output += "<td>" + admin.getAddress() + "</td>";
+//				output += "<td>" + admin.getEmail()+ "</td>";
+//				output += "<td>" + admin.getPhone()+ "</td>";
+//				output += "<td>" + admin.getPassword() + "</td>";
+//				output += "<td>" + admin.getAmount() + "</td>";
+//				output += "<td>" + admin.getLisencenum() + "</td>";
+//				
+//				
+//			}
+//			con.close();
+//			// Complete the html table
+//			output += "</table>";
+//
+//		} catch (Exception e) {
+//			output = "Error while reading the userTypes Details.";
+//			System.err.println(e.getMessage());
+//		}
+//
+//		return output;
+//	}
+//	
+//	
+	
+	
+	public String viewAdmins() {
+
+		String output = "";
+		
+		Admin admin = new Admin();
+		
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>AdminID</th>" + "<th>Name</th> "
+					+ "<th>Company</th> " + "<th>Address</th>" + "<th>Email</th>" + "<th>Phone</th>"  + "<th>Password</th>"  + "<th>Amount</th>"  +  "<th>Lisencenum</th></tr>";
+
+			String query = "select * from admin";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			// iterate through the rows in the result set
+			while (rs.next()) {
+
+				admin.setUid(rs.getInt("userId"));
+				admin.setName(rs.getString("name"));
+				admin.setCompany(rs.getString("company"));
+				admin.setAddress(rs.getString("address"));
+				admin.setEmail(rs.getString("email"));
+				admin.setPhone(rs.getString("phone"));
+				admin.setPassword(rs.getString("password"));
+				admin.setAmount(rs.getString("amount"));
+				admin.setlisencenum(rs.getString("lisencenum"));
+
+				// Add into the html table
+				output += "<tr><td>" + admin.getUid() + "</td>";
+				output += "<td>" + admin.getName() + "</td>";
+				output += "<td>" + admin.getCompany() + "</td>";
+				output += "<td>" + admin.getAddress() + "</td>";
+				output += "<td>" + admin.getEmail()+ "</td>";
+				output += "<td>" + admin.getPhone()+ "</td>";
+				output += "<td>" + admin.getPassword() + "</td>";
+				output += "<td>" + admin.getAmount() + "</td>";
+				output += "<td>" + admin.getLisencenum() + "</td>";
+				
+				
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+
+		} catch (Exception e) {
+			output = "Error while reading the userTypes Details.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//========================== Add In To Appointment Types =========================
+	
+		
+	public String addAdmins(Admin admin) {
+
+			String output = "";
+			try {
+
+				Connection con = dbObj.connect();
+				if (con == null) {
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = " INSERT INTO admin ( name , company ,address,email,phone ,password,amount,lisencenum) VALUES (?, ?, ?, ? , ? , ?,?,?)";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setString(1, admin.getName());
+				preparedStmt.setString(2, admin.getCompany());
+				preparedStmt.setString(3, admin.getAddress());
+				preparedStmt.setString(4, admin.getEmail());
+				preparedStmt.setString(5, admin.getPhone());
+				preparedStmt.setString(6, admin.getPassword());
+				preparedStmt.setString(7, admin.getAmount());
+				preparedStmt.setString(8, admin.getLisencenum());
+				
+				
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				output = "Inserted successfully";
+
+			} catch (Exception e) {
+				output = "Error while inserting";
+				System.err.println(e.getMessage());
+			}
+
+			return output;
+		}
+	
+	
+	
+		//============================= Update Appointment Type ==============================
+		
+	
+//			public String updateAdmins(Admin admin) {
+//
+//				String output = "";
+//
+//				try {
+//					Connection con = dbObj.connect();
+//					if (con == null) {
+//						return "Error while connecting to the database for updating.";
+//					}
+//					// create a prepared statement
+//					String query = "UPDATE admin SET name=? , company=?, address=?, email=?, phone=? , password=? , amount =? , lisencenum=? WHERE adminId = ?";
+//					PreparedStatement preparedStmt = con.prepareStatement(query);
+//
+//					// binding values
+//
+//					
+//					preparedStmt.setString(1, admin.getName());
+//					preparedStmt.setString(2, admin.getCompany());
+//					preparedStmt.setString(3, admin.getAddress());
+//				    preparedStmt.setString(4, admin.getEmail());
+//				    preparedStmt.setString(5, admin.getPhone());
+//				    preparedStmt.setString(6, admin.getPassword());
+//				    preparedStmt.setString(7, admin.getAmount());
+//				    preparedStmt.setString(8, admin.getLisencenum());
+//				    preparedStmt.setDouble(9, admin.getAid());
+//					
+//					
+//					// execute the statement
+//					preparedStmt.execute();
+//					con.close();
+//					output = "Updated successfully [ ID : "+ admin.getAid()+" ]";
+//				} catch (Exception e) {
+//					output = "Error while updating the Admin Id " + admin.getAid();
+//					System.err.println(e.getMessage());
+//				}
+//				return output;
+//			}
+//
+//
+//			//============================= Delete Appointment Type ==============================	
+//		
+//			
+//			public String deleteAdmins(Admin admin) {
+//				String output = "";
+//				try {
+//
+//					Connection con = dbObj.connect();
+//					if (con == null) {
+//						return "Error while connecting to the database for deleting.";
+//					}
+//
+//					// create a prepared statement
+//					String query = "DELETE FROM admin WHERE adminId=?";
+//					PreparedStatement preparedStmt = con.prepareStatement(query);
+//
+//					// binding values
+//					 preparedStmt.setInt(1, admin.getAid());
+//					//preparedStmt.setInt(4, appBean.getAppointment_Id());
+//					// execute the statement
+//					preparedStmt.execute();
+//					con.close();
+//					output = "Deleted successfully [ Admin Id : "+admin.getAid()+" ]";
+//
+//				} catch (Exception e) {
+//
+//					output = "Error while deleting the  Admin Id :" + admin.getAid();
+//					System.err.println(e.getMessage());
+//				}
+//
+//				return output;
+//			}
+	
+	
+	
+	
+	
+	public String updateAdmins(Admin admin){
+
+		String output = "";
+
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			// create a prepared statement
+			String query = "UPDATE admin SET name=? , company=?, address=?, email=?, phone=? , password=? , amount =? , lisencenum=?   WHERE userId = ?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+
+			
+			
+			preparedStmt.setString(1, admin.getName());
+			preparedStmt.setString(2, admin.getCompany());
+			preparedStmt.setString(3, admin.getAddress());
+		    preparedStmt.setString(4, admin.getEmail());
+		    preparedStmt.setString(5, admin.getPhone());
+		    preparedStmt.setString(6, admin.getPassword());
+		    preparedStmt.setString(7, admin.getAmount());
+		    preparedStmt.setString(8, admin.getLisencenum());
+		    preparedStmt.setDouble(9, admin.getUid());
+			
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Updated successfully [ ID : "+admin.getUid()+" ]";
+		} catch (Exception e) {
+			output = "Error while updating the User Id " + admin.getUid();
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+
+	//============================= Delete Appointment Type ==============================	
+
+	
+	public String deleteAdmins(Admin admin) {
+		String output = "";
+		try {
+
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+
+			// create a prepared statement
+			String query = "DELETE FROM admin WHERE userId=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			 preparedStmt.setInt(1, admin.getUid());
+			//preparedStmt.setInt(4, appBean.getAppointment_Id());
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully [ User Id : "+admin.getUid()+" ]";
+
+		} catch (Exception e) {
+
+			output = "Error while deleting the  User Id :" + admin.getUid();
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+	
+	
+	
+	
+	
+}
---
+++ PAF/src/com/adminService.java
@@ -0,0 +1,200 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import Controller.adminController;
+import Model.Admin;
+import Model.Researcher;
+
+
+
+
+@Path("/admins")
+public class adminService {
+	
+	adminController admins = new adminController();
+
+	
+	// get all types
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readAllTypes() {
+		return admins.viewAdmins();
+	}
+
+
+	// add types
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String enterType(String TypeData) {
+		// Convert the input string to a JSON object
+		JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+	
+		Admin adm = new Admin();
+		
+		
+		adm.setName(djosnObj.get("name").getAsString());
+		adm.setCompany(djosnObj.get("company").getAsString());
+		adm.setAddress(djosnObj.get("address").getAsString());
+		adm.setEmail(djosnObj.get("email").getAsString());
+		adm.setPhone(djosnObj.get("phone").getAsString());
+		adm.setPassword(djosnObj.get("password").getAsString());
+		adm.setAmount(djosnObj.get("amount").getAsString());
+		adm.setlisencenum(djosnObj.get("lisencenum").getAsString());
+	   
+		// Read the values from the JSON object
+	
+		String output = admins.addAdmins(adm);
+		return output;
+
+	}
+
+	
+	
+//	
+//	// update Types
+//		@PUT
+//		@Path("/")
+//		@Consumes(MediaType.APPLICATION_JSON)
+//		@Produces(MediaType.TEXT_PLAIN)
+//		public String updateAppType(String TypeData) {
+//
+//			// Convert the input string to a JSON object
+//			JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+//			Admin adm = new Admin();
+//
+//			adm.setAid(djosnObj.get("adminId").getAsInt());
+//		    adm.setName(djosnObj.get("name").getAsString());
+//			adm.setCompany(djosnObj.get("company").getAsString());
+//			adm.setAddress(djosnObj.get("address").getAsString());
+//			adm.setEmail(djosnObj.get("email").getAsString());
+//			adm.setPhone(djosnObj.get("phone").getAsString());
+//			adm.setPassword(djosnObj.get("password").getAsString());
+//			adm.setAmount(djosnObj.get("amount").getAsString());
+//			adm.setlisencenum(djosnObj.get("lisencenum").getAsString());
+//			
+//			String output = admins.updateAdmins(adm);
+//			return output;
+//		}
+//	
+	
+		
+//		
+//		// update Types
+//			@PUT
+//			@Path("/")
+//			@Consumes(MediaType.APPLICATION_JSON)
+//			@Produces(MediaType.TEXT_PLAIN)
+//			public String updateAppType(String TypeData) {
+//
+//				// Convert the input string to a JSON object
+//				JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+//				Admin adm = new Admin();
+//
+//				adm.setAid(djosnObj.get("adminId").getAsInt());
+//				adm.setName(djosnObj.get("name").getAsString());
+//				adm.setCompany(djosnObj.get("company").getAsString());
+//				adm.setAddress(djosnObj.get("address").getAsString());
+//				adm.setEmail(djosnObj.get("email").getAsString());
+//				adm.setPhone(djosnObj.get("phone").getAsString());
+//				adm.setPassword(djosnObj.get("password").getAsString());
+//				adm.setAmount(djosnObj.get("amount").getAsString());
+//				adm.setlisencenum(djosnObj.get("lisencenum").getAsString());
+//			
+//				
+//				String output = admins.updateAdmins(adm);
+//				return output;
+//			}
+//		
+//		
+//		
+//	
+//
+//
+//		// delete Types
+//		
+//		@DELETE
+//		@Path("/")
+//		@Consumes(MediaType.APPLICATION_JSON)
+//		@Produces(MediaType.TEXT_PLAIN)
+//		public String deletetype(String TypeData) {
+//			// Convert the input string to a JSON object
+//			JsonObject doc = new JsonParser().parse(TypeData).getAsJsonObject();
+//
+////			JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+//			Admin adm = new Admin();
+//			
+//			// Read the value from the element <ID>
+//	    	adm.setAid(doc.get("adminId").getAsInt());
+//			//String id = doc.get("appointment_Id").getAsString();
+//			String output = admins.deleteAdmins(adm);
+//			return output;
+//		}
+//	
+		
+		
+		// update Types
+			@PUT
+			@Path("/")
+			@Consumes(MediaType.APPLICATION_JSON)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String updateAppType(String TypeData) {
+
+				// Convert the input string to a JSON object
+				JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+				Admin adm = new Admin();
+
+				adm.setUid(djosnObj.get("userId").getAsInt());
+				adm.setName(djosnObj.get("name").getAsString());
+				adm.setCompany(djosnObj.get("company").getAsString());
+				adm.setAddress(djosnObj.get("address").getAsString());
+				adm.setEmail(djosnObj.get("email").getAsString());
+				adm.setPhone(djosnObj.get("phone").getAsString());
+				adm.setPassword(djosnObj.get("password").getAsString());
+				adm.setAmount(djosnObj.get("amount").getAsString());
+				adm.setlisencenum(djosnObj.get("lisencenum").getAsString());
+			
+				
+				String output = admins.updateAdmins(adm);
+				return output;
+			}
+		
+		
+		
+
+
+			// delete Types
+			
+			@DELETE
+			@Path("/")
+			@Consumes(MediaType.APPLICATION_JSON)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String deletetype(String TypeData) {
+				// Convert the input string to a JSON object
+				JsonObject doc = new JsonParser().parse(TypeData).getAsJsonObject();
+
+//				JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+				Admin adm = new Admin();
+				
+				// Read the value from the element <ID>
+		    	adm.setUid(doc.get("userId").getAsInt());
+				//String id = doc.get("appointment_Id").getAsString();
+				String output = admins.deleteAdmins(adm);
+				return output;
+			}
+
+}
