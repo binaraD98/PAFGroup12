e1d0db1da8fde676b137b10bc035a4a28e00c9cb
Binara Dewapura <67158768+binaraD98@users.noreply.github.com>
2021-04-25 05:37:25 +0530

    Update Search by IDs

--- FinanceManagement/src/Controller/propController.java
+++ FinanceManagement/src/Controller/propController.java
@@ -60,5 +60,54 @@ public class propController {
 			
 			return output;
 			
-		}	
+		}
+	
+		//......................................Search by ID.....................................................................
+		
+		public String viewPropsById(int propID) {
+
+			String output = "";
+			
+			appProposals appProp = new appProposals();
+			
+			try {
+				Connection con = dbObj.connect();
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+				}
+				
+				output = "<table border=\"1\"><tr><th>Proposal ID</th>"
+						+ "<th>Researcher ID</th> "+" <th></th> "+" <th> Title</th>" + "<th>Expected Budget </th></tr>";
+
+				String query = "select * from appproposals where propID ='"+propID+"'  ";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+
+				// iterate through the rows in the result set
+				while (rs.next()) {
+
+					appProp.setPropID(rs.getInt("propID"));
+					appProp.setResearcherID(rs.getInt("researcherID"));
+					appProp.setTitle(rs.getString("title"));
+					appProp.setExpecBudge(rs.getDouble("expecBudge"));
+
+					
+					output += "<tr><td>" + appProp.getPropID() + "</td>";
+					output += "<td>" + appProp.getResearcherID() + "</td>";
+					output += "<td>" + appProp.getTitle() + "</td>";
+					output += "<td>" + appProp.getExpecBudge() + "</td></tr>";
+					
+					
+				}
+				con.close();
+				// Complete the html table
+				output += "</table>";
+
+			} catch (Exception e) {
+				output = "Error while reading the Order Details By Id.";
+				System.err.println(e.getMessage());
+			}
+
+			return output;
+		}
 }
